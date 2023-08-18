74eb0c453c773f451c73bb0821d7de3f79c71ec1
kkoodo <12151782@inha.edu>
2023-08-18 14:43:46 +0900

    Modifying procedure

--- ORACLE/procedure_record_job_log_ddl.sql
+++ ORACLE/procedure_record_job_log_ddl.sql
@@ -1,15 +1,17 @@
 CREATE OR REPLACE PROCEDURE RECORD_JOB_LOG
 (
-    vROW_NUM     IN NUMBER   := NULL
-  , vSTATUS      IN VARCHAR2 := NULL
-  , vSTEP        IN VARCHAR2 := NULL
-  , vERR_CD      IN VARCHAR2 := NULL
-  , vERR_MSG     IN VARCHAR2 := NULL
+    vLOG_ID     IN VARCHAR2 := NULL
+  , vSTATUS     IN VARCHAR2 := NULL
+  , vSTEP       IN VARCHAR2 := NULL
+  , vERR_CD     IN VARCHAR2 := NULL
+  , vERR_MSG    IN VARCHAR2 := NULL
 )
 IS
+    vEXISTS_ROW TBL_PRTC_LOG.LOG_ID%TYPE;
+    vREPET_CNT  NUMBER;
 BEGIN
-    IF vROW_NUM IS NULL THEN 
-        RAISE_APPLICATION_ERROR(-20001, 'CHECK ARGUMENT. NULL TO ROW_NUM');
+    IF vLOG_ID IS NULL THEN 
+        RAISE_APPLICATION_ERROR(-20001, 'CHECK ARGUMENT. NULL TO LOG_ID');
     END IF;        
     IF vSTATUS IS NULL THEN 
         RAISE_APPLICATION_ERROR(-20002, 'CHECK ARGUMENT. NULL TO STATUS');
@@ -18,33 +20,60 @@ BEGIN
         RAISE_APPLICATION_ERROR(-20003, 'CHECK ARGUMENT. NULL TO STEP');
     END IF;
 
-    CASE vSTATUS
-    WHEN 'START' THEN
-        INSERT INTO TBL_PRTC_LOG(ROW_NUM, STATUS, STEP, ST_TIME, UP_TIME, FN_TIME, ER_TIME, ERR_CD, ERR_MSG)
-               VALUES (vROW_NUM, 'START', vSTEP, SYSTIMESTAMP, NULL, NULL, NULL, NULL, NULL);
-    WHEN 'UPDATE' THEN
-        UPDATE TBL_PRTC_LOG L
-           SET L.STATUS = 'UPDATE'
-             , L.STEP = vSTEP
-             , L.UP_TIME = SYSTIMESTAMP
-         WHERE L.ROW_NUM = vROW_NUM;
-    WHEN 'FINISH' THEN
-        UPDATE TBL_PRTC_LOG L
-           SET L.STATUS = 'FINISH'
-             , L.STEP = vSTEP
-             , L.FN_TIME = SYSTIMESTAMP
-         WHERE L.ROW_NUM = vROW_NUM;
-    WHEN 'ERROR' THEN
-        UPDATE TBL_PRTC_LOG L
-           SET L.STATUS = 'ERROR'
-             , L.STEP = vSTEP
-             , L.ER_TIME = SYSTIMESTAMP
-             , L.ERR_CD = vERR_CD
-             , L.ERR_MSG = vERR_MSG
-         WHERE L.ROW_NUM = vROW_NUM;         
-    END CASE;
+    -- IF WRITE LIKE THIS, WHEN EXCEPTION CAUSES, THEN OTHER STEPO ARE IGNORED
+--    SELECT LOG_ID
+--      INTO vEXISTS_ROW
+--      FROM DUAL 
+--     WHERE EXISTS (SELECT * FROM TBL_PRTC_LOG WHERE LOG_ID = vLOG_ID);
+--    
+--    EXCEPTION
+--        WHEN NO_DATA_FOUND THEN vEXISTS_ROW := NULL;
+
+    SELECT MAX(LOG_ID) 
+      INTO vEXISTS_ROW
+      FROM TBL_PRTC_LOG 
+     WHERE EXISTS (SELECT * FROM TBL_PRTC_LOG WHERE LOG_ID = vLOG_ID AND STATUS IN ('FINISH', 'ERROR'));
     
-    COMMIT;
+    IF vEXISTS_ROW IS NULL THEN
+        CASE vSTATUS WHEN 'START' 
+                     THEN INSERT INTO TBL_PRTC_LOG(LOG_ID, STATUS, STEP, ST_TIME, UP_TIME, FN_TIME, ER_TIME, ERR_CD, ERR_MSG)
+                                 VALUES (vLOG_ID, vSTATUS, vSTEP, SYSTIMESTAMP, NULL, NULL, NULL, NULL, NULL);
+                     WHEN 'UPDATE' 
+                     THEN UPDATE TBL_PRTC_LOG L
+                             SET L.STATUS = vSTATUS
+                               , L.STEP = vSTEP
+                               , L.UP_TIME = SYSTIMESTAMP
+                           WHERE L.LOG_ID = vLOG_ID;
+                     WHEN 'FINISH' 
+                     THEN UPDATE TBL_PRTC_LOG L
+                             SET L.STATUS = vSTATUS
+                               , L.STEP = vSTEP
+                               , L.FN_TIME = SYSTIMESTAMP
+                           WHERE L.LOG_ID = vLOG_ID;
+                     WHEN 'ERROR' 
+                     THEN UPDATE TBL_PRTC_LOG L
+                             SET L.STATUS = vSTATUS
+                               , L.STEP = vSTEP
+                               , L.ER_TIME = SYSTIMESTAMP
+                               , L.ERR_CD = vERR_CD
+                               , L.ERR_MSG = vERR_MSG
+                           WHERE L.LOG_ID = vLOG_ID;         
+                     END CASE;
+                               
+        COMMIT;
+        
+    ELSIF vEXISTS_ROW IS NOT NULL AND vSTATUS = 'START' THEN
+        SELECT SUBSTR(ERR_MSG, INSTR(ERR_MSG, '(') + 1, INSTR(ERR_MSG, ')') - INSTR(ERR_MSG, '(') - 1)
+          INTO vREPET_CNT
+          FROM TBL_PRTC_LOG
+         WHERE LOG_ID = vLOG_ID;
+         
+        UPDATE TBL_PRTC_LOG L
+           SET L.ER_TIME = SYSTIMESTAMP
+             , L.ERR_MSG = 'THIS ROW ALREADY PROCESSED (' || TO_CHAR(NVL(vREPET_CNT, 0) + 1) || ') TIMES'
+         WHERE L.LOG_ID = vLOG_ID;
+         
+    END IF;
 
 END;
 /
--- ORACLE/procedure_run_job_ddl.sql
+++ ORACLE/procedure_run_job_ddl.sql
@@ -1,67 +1,75 @@
 CREATE OR REPLACE PROCEDURE RUN_JOB
 IS
-    LOG_ID          TBL_PRTC_BEFORE.ID%TYPE;
+    vLOG_ID         TBL_PRTC_LOG.LOG_ID%TYPE;
     CURSOR          ROW_CUR IS SELECT * FROM TBL_PRTC_BEFORE ORDER BY ROWID;
     vSQL            CLOB    := NULL;
+    vIS_EXIST       TBL_PRTC_AFTER.ID%TYPE;
 BEGIN
     
     FOR TBL_ROW IN ROW_CUR
     LOOP
-        LOG_ID := TBL_ROW.ID;
-        
-        -- STEP1. START RECORDING TBL_PRTC_LOG TABLE AND THE STATUS IS 'START'
-        RECORD_JOB_LOG(LOG_ID, 'START', '01');
-        
-        -- STEP2. SELECT ROW, AND PUT THEN INTO VARIABLE
-        RECORD_JOB_LOG(LOG_ID, 'UPDATE', '02');
-        
-        vSQL := 'INSERT INTO TBL_PRTC_AFTER (ID, NAME, USERNAME, EMAIL, PHONE, CONTENTS, UPDATE_TIME) VALUES' || CHR(10) || '(';
-        
-        -- STEP3. IF PERSONAL INFORMATION EXIST, THEN REPLACE AS '*', AND WRITE DINAMIC QUERY
-        RECORD_JOB_LOG(LOG_ID, 'UPDATE', '03');
-        
-        vSQL := vSQL || CHR(10) 
-                || '''' || LPAD(TBL_ROW.ID, 4, '0') || '''' || ', '
-                || '''' || SUBSTR(TBL_ROW.NAME, 1, INSTR(TBL_ROW.NAME, ' ')) || REGEXP_REPLACE(SUBSTR(TBL_ROW.NAME, INSTR(TBL_ROW.NAME, ' ') + 1), '[a-zA-Z]', '*') || '''' || ', '
-                || '''' || REGEXP_REPLACE(SUBSTR(TBL_ROW.USERNAME, INSTR(TBL_ROW.USERNAME, ' ') + 1), '[a-zA-Z]', '*') || '''' || ', '
-                || '''' || REGEXP_REPLACE(SUBSTR(TBL_ROW.EMAIL, 1, INSTR(TBL_ROW.EMAIL, '@')), '[a-zA-Z]', '*') || SUBSTR(TBL_ROW.EMAIL, INSTR(TBL_ROW.EMAIL, '@') + 1) || '''' || ', '
-                                -- EX : (775)976-6794 x41206
-                || '''' || CASE WHEN INSTR(TBL_ROW.PHONE, '(') > 0 AND INSTR(TBL_ROW.PHONE, '-', 1) > 0
-                                THEN SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), ')')) 
-                                     || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), ')') + 1, INSTR(TRIM(TBL_ROW.PHONE), '-') - INSTR(TRIM(TBL_ROW.PHONE), ')') - 1), '[0-9]', '*')
-                                     || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-'))
-                                -- EX : 586.493.6943 x140
-                                WHEN INSTR(TBL_ROW.PHONE, '(') = 0 AND INSTR(TBL_ROW.PHONE, '-', 1) = 0
-                                THEN SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), '.', 1))
-                                     || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '.', 1) + 1, INSTR(TRIM(TBL_ROW.PHONE), '.', 2)), '[0-9]', '*')
-                                     || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '.', 2) + 1)
-                                -- EX : 493-170-9623 x156
-                                ELSE SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), '-', 1))
-                                     || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-', 1) + 1, INSTR(TRIM(TBL_ROW.PHONE), '-', 2)), '[0-9]', '*')
-                                     || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-', 2) + 1)
-                                END || '''' || ', '
-                || 'NULL , '
-                || 'SYSTIMESTAMP'
-                || CHR(10) || ')';
+        SELECT DUPLICATE_CHECK(TBL_ROW.ID)
+          INTO vIS_EXIST
+          FROM DUAL;
+          
+        IF vIS_EXIST IS NULL THEN
+            vLOG_ID := LPAD(TBL_ROW.ID, 4, '0');
+            
+            -- STEP1. START RECORDING TBL_PRTC_LOG TABLE AND THE STATUS IS 'START'
+            RECORD_JOB_LOG(vLOG_ID, 'START', '01');
+            
+            -- STEP2. SELECT ROW, AND PUT THEN INTO VARIABLE
+            RECORD_JOB_LOG(vLOG_ID, 'UPDATE', '02');
+            
+            vSQL := 'INSERT INTO TBL_PRTC_AFTER (ID, NAME, USERNAME, EMAIL, PHONE, CONTENTS, UPDATE_TIME) VALUES' || CHR(10) || '(';
+            
+            -- STEP3. IF PERSONAL INFORMATION EXIST, THEN REPLACE AS '*', AND WRITE DINAMIC QUERY
+            RECORD_JOB_LOG(vLOG_ID, 'UPDATE', '03');
+            
+            vSQL := vSQL || CHR(10) 
+                    || '''' || LPAD(TBL_ROW.ID, 4, '0') || '''' || ', '
+                    || '''' || SUBSTR(TBL_ROW.NAME, 1, INSTR(TBL_ROW.NAME, ' ')) || REGEXP_REPLACE(SUBSTR(TBL_ROW.NAME, INSTR(TBL_ROW.NAME, ' ') + 1), '[a-zA-Z]', '*') || '''' || ', '
+                    || '''' || REGEXP_REPLACE(SUBSTR(TBL_ROW.USERNAME, INSTR(TBL_ROW.USERNAME, ' ') + 1), '[a-zA-Z]', '*') || '''' || ', '
+                    || '''' || REGEXP_REPLACE(SUBSTR(TBL_ROW.EMAIL, 1, INSTR(TBL_ROW.EMAIL, '@')), '[a-zA-Z]', '*') || SUBSTR(TBL_ROW.EMAIL, INSTR(TBL_ROW.EMAIL, '@') + 1) || '''' || ', '
+                                    -- EX : (775)976-6794 x41206
+                    || '''' || CASE WHEN INSTR(TBL_ROW.PHONE, '(') > 0 AND INSTR(TBL_ROW.PHONE, '-', 1) > 0
+                                    THEN SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), ')')) 
+                                         || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), ')') + 1, INSTR(TRIM(TBL_ROW.PHONE), '-') - INSTR(TRIM(TBL_ROW.PHONE), ')') - 1), '[0-9]', '*')
+                                         || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-'))
+                                    -- EX : 586.493.6943 x140
+                                    WHEN INSTR(TBL_ROW.PHONE, '(') = 0 AND INSTR(TBL_ROW.PHONE, '-', 1) = 0
+                                    THEN SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), '.', 1))
+                                         || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '.', 1) + 1, INSTR(TRIM(TBL_ROW.PHONE), '.', 2)), '[0-9]', '*')
+                                         || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '.', 2) + 1)
+                                    -- EX : 493-170-9623 x156
+                                    ELSE SUBSTR(TRIM(TBL_ROW.PHONE), 1, INSTR(TRIM(TBL_ROW.PHONE), '-', 1))
+                                         || REGEXP_REPLACE(SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-', 1) + 1, INSTR(TRIM(TBL_ROW.PHONE), '-', 2)), '[0-9]', '*')
+                                         || SUBSTR(TRIM(TBL_ROW.PHONE), INSTR(TRIM(TBL_ROW.PHONE), '-', 2) + 1)
+                                    END || '''' || ', '
+                    || 'NULL , '
+                    || 'SYSTIMESTAMP'
+                    || CHR(10) || ')';
+            
+    --        DBMS_OUTPUT.PUT_LINE('vSQL : ' || CHR(10) || vSQL);
+            
+            -- STEP4. IF WRITING QUERY FINISHED, UPDATE LOG'S STATUS TO 'UPDATE' AND EXECUTE DINAMIC QUERY
+            RECORD_JOB_LOG(vLOG_ID, 'UPDATE', '04');
+            
+            EXECUTE IMMEDIATE vSQL;
+            
+            COMMIT;
 
-        
-        DBMS_OUTPUT.PUT_LINE('vSQL : ' || CHR(10) || vSQL);
-        
-        -- STEP4. IF WRITING QUERY FINISHED, UPDATE LOG'S STATUS TO 'UPDATE' AND EXECUTE DINAMIC QUERY
-        RECORD_JOB_LOG(LOG_ID, 'UPDATE', '04');
-        EXECUTE IMMEDIATE vSQL;        
-        
-        COMMIT;
-        
-        -- STEP5. IF EXECUTE COMMIT, THEN UPDATE LOG'S STATUS TO 'FINISH'
-        RECORD_JOB_LOG(LOG_ID, 'FINISH', '05');
+            -- STEP5. IF EXECUTE COMMIT, THEN UPDATE LOG'S STATUS TO 'FINISH'
+            RECORD_JOB_LOG(vLOG_ID, 'FINISH', '05');
+            
+        END IF;        
         
     END LOOP;
     
     -- STEP6. IF ERROR OCCURED, THEN CALL RECORD_JOB_LOG, AND UPDATE LOG'S STATUS TO 'ERROR'
     EXCEPTION
-    WHEN OTHERS THEN ROLLBACK;
-    RECORD_JOB_LOG(LOG_ID, 'ERROR', '06', SQLCODE, SUBSTR(SQLERRM, 12));
+        WHEN OTHERS THEN ROLLBACK;
+        RECORD_JOB_LOG(vLOG_ID, 'ERROR', '06', SQLCODE, SUBSTR(SQLERRM, 12));
     
 END;
 /
