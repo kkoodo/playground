2d55b6dd83231db226778cd46b9a256a53946c35
kkoodo <12151782@inha.edu>
2026-03-20 15:05:14 +0900

    [20260320] add new lecture

---
+++ VUE/learn-vue3-kdy/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+
+export default {
+	setup() {
+		const counter = ref(0);
+		const increment = () => counter.value++;
+
+		onMounted(() => {
+			console.log('마운트 됫지롱!');
+		});
+
+		return {
+			counter,
+			increment,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
