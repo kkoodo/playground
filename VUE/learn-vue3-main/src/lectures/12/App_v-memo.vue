46ee7f757864f9bc6664010b6d1a7517c1d037e9
kkoodo <12151782@inha.edu>
2026-03-20 19:56:01 +0900

    [20260320] add "v-cloak" and "v-memo" descriptions

--- VUE/learn-vue3-main/src/lectures/12/App_v-memo.vue
+++ VUE/learn-vue3-main/src/lectures/12/App_v-memo.vue
@@ -27,6 +27,15 @@ export default {
 		return { subscribers, views, likes };
 	},
 };
+/*
+[v-memo]
+- 역할 : 렌더링 성능 최적화 (Vue 3.2+)
+	React의 useMemo와 비슷한 역할입니다.
+	화면의 특정 부분(DOM 트리)의 렌더링 결과를 '기억(캐싱)'해 두었다가, 
+	내가 지정한 값이 바뀔 때만 다시 그리도록 통제하는 강력한 최적화 도구입니다.
+- 동작 원리: 배열 안에 추적할 조건 값을 넣습니다. 
+	그 값이 그대로면 Vue는 아예 가상 DOM 비교조차 건너뛰고 이전 렌더링 결과를 재사용합니다.
+*/
 </script>
 
 <style lang="scss" scoped></style>
