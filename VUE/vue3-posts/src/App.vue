dc343f981da13471585315c8aef4e250aae9a30a
kkoodo <12151782@inha.edu>
2026-03-24 21:59:18 +0900

    [20260324] class 5,6 create project and setting

---
+++ VUE/vue3-posts/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeader from './layouts/TheHeader.vue';
+import TheVue from './layouts/TheVue.vue';
+</script>
+
+<template>
+	<TheHeader></TheHeader>
+	<TheVue></TheVue>
+</template>
+
+<style></style>
